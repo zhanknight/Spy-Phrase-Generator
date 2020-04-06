fbf8d4be9a54d84aac0da520682bda8e5cd894f0
zhanknight <zhanknight@gmail.com>
2020-04-06 07:47:57 -0400

    added copy button

--- SpyPhraseGenerator.js
+++ SpyPhraseGenerator.js
@@ -1,18 +1,20 @@
 var subjectList = ["The Eagle has", "The Owl has", "The Arrow has", "The butterflies have", "Our bundle of joy has", "A dozen eggs have",
     "Fifty red balloons have", "The puffy clouds have", "A lovely swan has", "The last bottle of wine has", "The cruel fox has",
-    "Mother has", "Father has", "Your ex lover has", "Brother has", "Sister has", "The doctor has", "The nurses have"];
+    "Mother has", "Father has", "Your ex lover has", "Brother has", "Sister has", "The doctor has", "The nurses have", "The bacon has"];
 
 var verbList = [" landed.", " arrived.", " made the grade.", " taken the bait.", " eaten the bacon.", " seized the day!", " painted the town.",
     " spoiled the milk.", " planned a picnic.", " licked the stamps.", " played the piano.", " melted the ice.", " given the medicine",
-    " taken the pills", " given permission.", " reached the destination.", " acquired immunity.", " baked the potato.", " cut the cake!"];
+    " taken the pills", " given permission.", " reached the destination.", " acquired immunity.", " baked the potato.", " cut the cake!", " sizzled."];
 
 
 // get textbox for new phrases and generator button
 var phraseZone = document.querySelector("#phraseZone");
 var generatorButton = document.querySelector("#generatorButton");
+var copyButton = document.querySelector("#copyButton");
 
-// add listener to button
+// add listener to buttons
 generatorButton.addEventListener("click", generatePhrase);
+copyButton.addEventListener("click", copyPhrase);
 
 // make function that does the generating and changing display
 function generatePhrase() {
@@ -24,10 +26,16 @@ function generatePhrase() {
     phraseZone.textContent = subject + verb;
 }
 
-
-
-
-
-// add sharing buttons?!
+// make a function to copy the generated phrase
+function copyPhrase() {
+    var copyText = phraseZone.textContent;
+    var tempText = document.createElement('textarea');
+    tempText.value = copyText;
+    document.body.appendChild(tempText);
+    tempText.select(); 
+    document.execCommand("copy");
+    document.querySelector("textarea").remove();
+    alert("Copied the Spy Phrase!");
+  }
 
 
